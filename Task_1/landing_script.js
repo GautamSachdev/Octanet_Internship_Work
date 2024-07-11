a22cd389209d87062dce81dfeeeadf47774c81d1
Gautam Sachdev <gautamsachdev6@gmail.com>
2024-07-11 11:06:02 +0530

    Add files via upload

---
+++ Task_1/landing_script.js
@@ -0,0 +1,5 @@
+// Add event listener to subscribe button
+document.querySelector('#subscribe button').addEventListener('click', function() {
+    // Replace with your YouTube channel URL
+    window.location.href = 'https://www.youtube.com/channel/UCIZa05AzCi-iAgM1iduH-fg';
+});
